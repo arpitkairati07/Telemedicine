8488218e34ca8c95dd9457b5881e12a5a5ddb05b
Arpit9026 <arpitkairati54@gmail.com>
2025-06-14 11:15:44 +0530

    Navbar Responsiveness

--- Frontend Telemedicine/src/Components/Header.jsx
+++ Frontend Telemedicine/src/Components/Header.jsx
@@ -1,39 +1,27 @@
 import React from "react";
-import { assets } from "../assets/assets_frontend/assets";
+import BannerImage from "../assets/assets_frontend/Banner.jpg";
 
 const Header = () => {
   return (
-    <div className="flex flex-col md:flex-row flex-wrap bg-[#5f6FFF] rounded-lg px-6 md:px-10 lg:px-20">
-      {/* --------------------------Left Side of the Header--------------- */}
-      <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]">
-        <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
-          Book Appointment <br /> with Trusted Doctors
-        </p>
-        <div className="flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light">
-          <img className="w-28" src={assets.group_profiles} alt="" />
-          <p>
-            Simply browse through our extensive list of trusted doctors,
-            <br className="hidden sm:block" />
-            schedule your appointment hastle free.
-          </p>
-        </div>
+    <section
+      className="relative w-full h-[65vh] md:h-[85vh] bg-center bg-cover bg-no-repeat flex items-center px-6 md:px-16 lg:px-24 rounded-2xl"
+      style={{ backgroundImage: `url(${BannerImage})` }}
+    >
+      <div className="max-w-3xl">
+        {/* Main Heading */}
+        <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold leading-tight text-[#e0eaff] drop-shadow-sm">
+          Healing Begins <br /> With Compassion
+        </h1>
+
+        {/* CTA Button */}
         <a
           href="#speciality"
-          className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 tranisition-all duration-300"
+          className="inline-block mt-6 px-6 py-3 bg-white text-gray-800 text-sm md:text-base font-semibold rounded-full hover:scale-105 transition-transform duration-300"
         >
-          Book Appointement{" "}
-          <img className="w-3" src={assets.arrow_icon} alt="Arrow Icon" />
+          Book Appointment
         </a>
       </div>
-      {/* ----------------Right Side of the Header ----------------*/}
-      <div className="md:w-1/2 relative ">
-        <img
-          className="w-full md:absolute bottom-0 h-auto rounded-lg"
-          src={assets.header_img}
-          alt=""
-        />
-      </div>
-    </div>
+    </section>
   );
 };
 
