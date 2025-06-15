030cec6b325d9686da029ce694a2e06ba7c88df6
Arpit9026 <arpitkairati54@gmail.com>
2025-06-15 11:37:36 +0530

    Backend

---
+++ Backend Telemedicine/server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+
+// App Config
+const app=express();
+const port=process.env.PORT || 4000;
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+connectDB()
+connectCloudinary()
+
+// API endpoints
+app.get('/',(req,res)=>{
+    res.send('api working')
+})
+
+app.listen(port,()=>console.log(`Server is running on the port ${port}`));
